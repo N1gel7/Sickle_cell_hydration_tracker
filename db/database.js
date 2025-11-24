0fdf18a7e6055e14c8c35e30e3d7561aeea0ad9e
N1gel7 <nigelnortey123@gmail.com>
2025-11-24 12:57:05 +0000

    db code

---
+++ db/database.js
@@ -0,0 +1,24 @@
+import Database  from "better-sqlite3"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const db = new Database(join(__dirname, 'hydration.db'));
+
+export const initDatabase = () =>{
+    db.exec(`
+       CREATE TABLE IF NOT EXISTS hydration_entries(
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       amount_litres REAL NOT NULL,
+       timestamp TEXT NOT NULL,
+       drink_type TEXT DEFAULT,
+       notes TEXT,
+       created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+        `);
+        console.log("Database initialised");
+};
+
+export default db;
